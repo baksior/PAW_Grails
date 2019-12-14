7ede8c8d7bf41fec2f1cbeefdecdbb50071331c8
Jędrek <j-szelag@gazeta.pl>
2019-12-14 20:19:08 +0100

    register fix

--- Trello/grails-app/controllers/trello/UserController.groovy
+++ Trello/grails-app/controllers/trello/UserController.groovy
@@ -7,33 +7,12 @@ import grails.plugin.springsecurity.annotation.Secured
 
 class UserController extends RestfulController {
     static responseFormats = ['json', 'xml']
-    static allowedMethods = [register: "POST"]
 
     UserController() {
         super(User)
     }
 
     def springSecurityService
-
-    @Secured('permitAll')
-    def register(){
-
-        def u = User.findByUsername(request.JSON.username)
-        if(u == null){
-            def newUser = new User(request.JSON.username, request.JSON.password).save()
-            def role = Role.findByAuthority('ROLE_USER')
-
-            UserRole.create newUser, role
-
-                UserRole.withTransaction { status ->
-                    UserRole.withSession {
-                        it.flush()
-                        it.clear()
-                    }
-                }
-
-        }
-    }
 
     @Secured('ROLE_USER')
     def getBoards() {
